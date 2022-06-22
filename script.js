455b5f68b84d07f5a691fd2f1ed40277e9733f10
Mikhail Malisov <malisov.mikhail@gmail.com>
2022-06-22 20:47:54 +0300

    add pics + make prettier

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
+const diceImg = document.querySelector('#diceImg') // dice image
 roll_num = 0 // setting the roll counter at 0
-document.querySelector('#roll').onclick = function() { // the following happens when clicking "roll"
-    let dice_num = (document.querySelector('#dice_num').value); // number of dice to be rolled
-    let dice = (document.querySelector('#dice').selectedOptions[0].value); // type of dice to be rolled
-    roll_num++; // increasing roll counter
 
-    roll_values = new Array // setting up the array to store the rolls of individual dice
-    roll_values.length = dice_num
-    roll_values.fill(0)
+document.querySelector('#roll').onclick = function () {
+  // the following happens when clicking "roll"
+  let dice_num = document.querySelector('#dice_num').value // number of dice to be rolled
+  let dice = document.querySelector('#dice').selectedOptions[0].value // type of dice to be rolled
+  roll_num++ // increasing roll counter
 
-    let get_roll_value = function() { // this function rolls a specified dice
-        roll_value = Math.floor(Math.random() * dice + 1)
-        return roll_value
-     }
+  roll_values = new Array() // setting up the array to store the rolls of individual dice
+  roll_values.length = dice_num
+  roll_values.fill(0)
 
-     roll_values.forEach(function(item, index, array){ // this calls the get_roll_value function for each element within the array
-          array[index] = get_roll_value(item);
-     });
+  let get_roll_value = function () {
+    // this function rolls a specified dice
+    roll_value = Math.floor(Math.random() * dice + 1)
+    return roll_value
+  }
 
-    let rolls_sum = roll_values.reduce((a, b) => a + b) // this calculates the sum of rolls of individual dice
+  roll_values.forEach(function (item, index, array) {
+    // this calls the get_roll_value function for each element within the array
+    array[index] = get_roll_value(item)
+  })
 
-    let roll = document.createElement('div'); // creating a div to append
-    roll.innerHTML = `Roll #${roll_num} (${dice_num}d${dice}): ${rolls_sum} (dice: ${roll_values})` // the div's contents
-    document.getElementById('place_for_text').appendChild(roll) // appending the div
-    }
+  let rolls_sum = roll_values.reduce((a, b) => a + b) // this calculates the sum of rolls of individual dice
 
-document.querySelector('#clear').onclick = function() { // this function removes all the divs created
-    roll_num = 0 // resetting the counter after clearing roll history
-    let to_remove = document.getElementById('place_for_text')
-    to_remove.replaceChildren();
+  let roll = document.createElement('div') // creating a div to append
+  roll.innerHTML = `Roll #${roll_num} (${dice_num}d${dice}): ${rolls_sum} (dice: ${roll_values})` // the div's contents
+  document.getElementById('result').appendChild(roll) // appending the div
 }
+
+document.querySelector('#clear').onclick = function () {
+  // this function removes all the divs created
+  roll_num = 0 // resetting the counter after clearing roll history
+  let to_remove = document.getElementById('result')
+  to_remove.replaceChildren()
+}
+
+// changing dice image according to selected dice
+document.querySelector('#dice').addEventListener('change', () => {
+  let d = document.querySelector('#dice').selectedOptions[0].value
+  diceImg.src = `pics/d${d}.jpg`
+})
